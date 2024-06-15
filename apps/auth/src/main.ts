ceaf85b4d8f4f41628209d319a5ca4aa7ea2f612
anasabbal <anas.abbal66@gmail.com>
2024-06-15 16:38:17 +0100

    implemente consumer and producer using rabiit for sending email as notification

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: 3004,
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
