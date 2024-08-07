b5f14a2e407dd7afc7dcbedc0c4f8384cc5f1716
Anas Abbal <anas.abbal66@gmail.com>
2024-08-07 16:37:05 +0100

    add some tests

--- apps/notification/src/notification.module.ts
+++ apps/notification/src/notification.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forFeature([]),
+    DatabaseModule.forRoot(
+      process.env.DATABASE_URI,
+      process.env.DATABASE_NAME,
+      process.env.DATABASE_TYPE as 'mongodb' | 'postgres'
+    ),
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
